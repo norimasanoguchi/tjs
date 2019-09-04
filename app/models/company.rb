b78d549b2c671d160673e98503e9435d0f49b7b7
norimasanoguchi <norimasanoguchi@gmail.com>
2019-09-05 07:47:35 +0900

    Update:Companyモデルバリデーション追加

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,9 @@
 class Company < ApplicationRecord
   authenticates_with_sorcery!
+  validates :name,  presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :contact_person, presence: true
+  validates :tel, presence: true
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
